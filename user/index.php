1392acef723262fe769f08c3387b0a98bd54f573
Catt3e <135534799+Catt3e@users.noreply.github.com>
2024-05-07 13:51:55 +0700

    Update index.php

--- user/index.php
+++ user/index.php
@@ -1,185 +1,199 @@
+<?php
+// Bắt đầu session
+include(__DIR__ . '/../lib/session.php');
+include(__DIR__ . '/../lib/database.php');
+Session::startSession();
+
+//Đăt múi giờ Việt Nam cho các datetime
+date_default_timezone_set('Asia/Ho_Chi_Minh');
+
+
+?>
 <!DOCTYPE html>
 <html lang="vi">
 
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Trang chủ</title>
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.min.js"></script>
+    <!-- <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script> -->
+    <link rel="stylesheet" href="https://code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
+    <script src="jsUser/comment.js"></script>
+    <script src="jsUser/productView.js"></script>
+    <script src="jsUser/cart.js"></script>
+    <script src="jsUser/infoUser.js"></script>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
-    <title>Danh sách sản phẩm</title>
+
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+    <link rel="stylesheet" href="style/css1.css">
+    <link rel="stylesheet" href="style/productViewUser.css">
+    <link rel="stylesheet" href="style/product_detail.css">
+    <link rel="stylesheet" href="style/cartStyle.css">
+    <!-- <link rel="stylesheet" href="style/homeStyle.css"> -->
+    <!-- <link rel="stylesheet" href="style/productViewStyle.css"> -->
+    <!-- <link rel="stylesheet" href="style/SlideStyle.css"> -->
+
+
+    <style>
+        .notification {
+            display: none;
+            position: fixed;
+            top: 30%;
+            left: 50%;
+            transform: translate(-50%, -50%);
+            background-color: mediumseagreen;
+            color: white;
+            padding: 10px 20px;
+            border-radius: 300px;
+            z-index: 9999;
+            transition: top 0.5s ease-out;
+        }
+
+        .notification.show {
+            display: block;
+        }
+
+        .loading-bar {
+            height: 4px;
+            background-color: #fff;
+            width: 0%;
+            animation: loading 0.5s linear;
+        }
+
+        @keyframes loading {
+            0% {
+                width: 0%;
+            }
+
+            100% {
+                width: 100%;
+            }
+        }
+        .icon{
+            width: 25px;
+            height: 25px;
+            margin-right: -7px;
+        }
+        .icon img{
+            width: 100%;
+            height: 100%;
+            object-fit: cover;
+            filter: invert(1);
+            transition: all 0.3s ease-in-out;
+        }
+        .tag:hover img{
+            transform: translateY(-7px) rotate(15deg) scale(1.3);
+        }
+        .tag{
+            display: flex;
+            flex-direction: row;
+            padding: 5px;
+        }
+    </style>
+
 </head>
 
-
-<style>
-
-</style>
-
 <body>
-    <div class="productViewUser">
-        <!-- <div id="slideshow">
-        <div class="slide-wrapper">
-            <div class="slide"><img src="../image/Banner1.png"></div>
-            <div class="slide"><img src="../image/Banner2.png"></div>
-        </div>
-        </div> -->
-
-        <video autoplay muted loop id="bg-video">
-            <source src="../image/vd3.mp4" type="video/mp4">
-            Your browser does not support HTML5 video.
-        </video>
-
-        <div class="product-container">
-            <h1 class="product-list-title lead">Danh sách sản phẩm</h1>
-
-            <div class="price-MintoMaxlink">
-                <form action="index.php" method="GET">
-                    <input type="hidden" name="ctrl" value="productControllerUser">
-                    <label for="price_min">Tìm kiếm theo khoảng giá từ:</label>
-                    <input type="number" name="price_min" id="price_min" min="0" required><br>
-                    <label for="price_max">đến:</label>
-                    <input type="number" name="price_max" id="price_max" min="0" required><br>
-                    <input type="submit" value="Xác nhận">
-                </form>
-            </div>
-
-            <div class="category-list">
-                <ul class="category-items">Danh mục sản phẩm: 
-                    <li class="category-item"><a href="index.php?ctrl=productControllerUser" class="category-link">tất cả</a></li>
-                    <?php
-                    if (!empty($categories)) {
-                        foreach ($categories as $category) {
-                    ?>
-                            <li class="category-item">
-                                <a href="index.php?ctrl=productControllerUser&action=index&id=<?php echo $category['id']; ?>&filter=category" class="category-link">
-                                    <?php echo $category['ten']; ?>
-                                </a>
-                            </li>
-                        <?php
-                        }
-                    } else {
-                        ?>
-                        <li class="category-item">Không có danh mục nào để hiển thị.</li>
-                    <?php
-                    }
-                    ?>
-                </ul>
-            </div>
-
-            <div class="d-flex align-items-center mb-3 product-search-handler">
-                <div id="sortAscOrDesc" class="sortAscOrDesc me-3">
-                    <form action="index.php" method="GET">
-                        <input type="hidden" name="ctrl" value="productControllerUser">
-                        <select name="action" class="form-select">
-                            <option value="index">Mặc định</option>
-                            <option value="sortProductsByPriceAsc">Sắp xếp tăng dần</option>
-                            <option value="sortProductsByPriceDesc">Sắp xếp giảm dần</option>
-                        </select>
-                        <input type="submit" value="Sắp xếp" class="btn btn-primary">
-                    </form>
-                </div>
-
-                <!-- Tìm kiếm sản phẩm theo tên -->
-                <form class="search-form-productUser">
-                    <input type="text" id="search" name="search" placeholder="Tìm kiếm sản phẩm" class="form-control me-2">
-                    <input type="submit" value="Tìm kiếm" class="btn btn-primary">
-                </form>
-            </div>
-
-            <p style="color: white;">Lọc khoảng giá bằng slider từ: <span id="price_min_value"></span> đến: <span id="price_max_value"></span></p>
-            <div id="price_slider"></div>
 
 
-            <div class="product-list" id="product-list">
+    <!-- Container Hiển thị thông báo khi thêm vào giỏ hàng -->
+    <div id="notification" class="notification">
+        <span id="notification-text"></span>
+        <div class="loading-bar"></div>
+    </div>
+
+
+
+    <div class="header">
+        <a href="index.php?ctrl=productControllerUser" class="logo">
+            <img src="../image/logo.jpg"  alt="" height=100px width=100px>
+        </a>
+        <div class="menu">
+            <ul class="menu-list">
+                <!-- <li><a href="index.php?ctrl=productControllerUser" class="menu-item">Trang chủ</a></li> -->
                 <?php
-                if (!empty($products)) {
-                    foreach ($products as $product) {
-                ?>
-                        <div class="product">
-                            <h2 class="product-name"><?php echo $product['ten']; ?></h2>
-                            <a href="index.php?ctrl=productControllerUser&action=detail&id=<?php echo $product['id']; ?>" class="product-image-link">
-                                <img src="/image/<?php echo $product['anh']; ?>" alt="Hình ảnh sản phẩm" width="200"><br>
-                            </a>
-                            <p>-----------------</p>
-                            <p class="product-price">Giá: <?php echo $product['gia']; ?> đ</p>
-                            <p class="product-stock">Còn lại: <?php echo $product['so_luong']; ?></p>
-                            <!-- <p class="product-description">Mô tả: <?php echo $product['mo_ta']; ?></p> -->
-                            <a href="#" class="buy-button" data-product-id="<?php echo $product['id']; ?>" data-product-status="<?php echo $product['so_luong'] > 0 ? 'in-stock' : 'out-of-stock'; ?>">
-                                <?php echo $product['so_luong'] > 0 ? 'Thêm vào giỏ hàng' : 'Tạm hết hàng'; ?>
-                            </a>
+                // Kiểm tra xem người dùng đã đăng nhập hay chưa
+                $ten_dang_nhap = Session::getSessionValue('ten_dang_nhap');
+                $id_khach_hang = Session::getSessionValue('id_khach_hang');
+                $vai_tro = Session::getSessionValue('vai_tro');
 
-                        </div>
-                    <?php
+                // Lấy danh sách quyền từ session
+
+                if ($ten_dang_nhap) {
+                    echo '<li><a href="index.php?ctrl=customerUserController" class="menu-item" >Tên:' . $ten_dang_nhap . " - " . $vai_tro . '</a></li><br>';
+                    echo '<li><a href="/login/index.php?ctrl=loginController&action=logout" class="menu-item" onclick="return confirm(\'Bạn có chắc chắn muốn đăng xuất không?\');">Đăng xuất</a></li>';
+                    if ($vai_tro !== 'Khách hàng') {
+                        echo '<li><a href="/admin/index.php" class="menu-item">Quản lý</a></li>';
                     }
                 } else {
-                    ?>
-                    <p class="no-products">Không có sản phẩm nào để hiển thị.</p>
-                <?php
+                    echo '<li><a href="/login/index.php?ctrl=loginController" class="menu-item">Đăng nhập</a></li>';
                 }
                 ?>
-            </div>
-
-            <div class="paginationProduct">
-                <?php
-                // Tính tổng số trang
-                $total_pages = ceil($total_products / $item_per_page); //ceil là hàm làm tròn lên
-                // Số trang ở đầu và cuối danh sách
-                $num_links_side = 2;
-                // Hiển thị nút Previous
-                if ($current_page > 1) {
-                    echo "<a href='#' data-page='" . ($current_page - 1) . "'>&laquo;</a>";
-                }
-                // Hiển thị trang đầu tiên
-                if ($current_page > ($num_links_side + 1)) {
-                    echo "<a href='#' data-page='1'>1</a>";
-                    echo "<span>...</span>";
-                }
-
-                // Hiển thị các trang giữa
-                for ($i = max(1, $current_page - $num_links_side); $i <= min($current_page + $num_links_side, $total_pages); $i++) {
-                    if ($i == $current_page) {
-                        echo "<a href='#' data-page='$i' style='background-color: white; color: black;'>$i</a>";
-                    } else {
-                        echo "<a href='#' data-page='$i'>$i</a>";
-                    }
-                }
-
-
-
-                // Hiển thị trang cuối cùng
-                if ($current_page < ($total_pages - $num_links_side)) {
-                    echo "<span>...</span>";
-                    echo "<a href='#' data-page='$total_pages'>$total_pages</a>";
-                }
-
-                // Hiển thị nút Next
-                if ($current_page < $total_pages) {
-                    echo "<a href='#' data-page='" . ($current_page + 1) . "'>&raquo;</a>";
-                }
-                ?>
-            </div>
+                <li class="tag"> 
+                    <div class="icon"><img src="/admin/img/icon/cart2.png" alt="cart"></div>
+                    <a href="index.php?ctrl=cartController&action=showCart" class="menu-item">Giỏ hàng</a>
+                </li>
+            </ul>
         </div>
 
-
-
-
-        <script>
-            // Lấy giá trị từ PHP và gán vào biến JavaScript để sử dụng cho ajax productView.js
-            var maxPriceFromPHP = <?php echo $maxPrice; ?>;
-        </script>
-
-        <!-- Container Hiển thị thông báo khi thêm vào giỏ hàng -->
-        <div id="notification" class="notification">
-            <span id="notification-text"></span>
-            <div class="loading-bar"></div>
-        </div>
     </div>
+    <div class="content">
+        <?php
+        if (!isset($_GET['ctrl'])) {
+            include 'controller/productControllerUser.php';
+        } else {
+            $ctrl = $_GET['ctrl'];
+            include 'controller/' . $ctrl . '.php';
+        }
+        ?>
+    </div>
+    <footer id="footer" class="bg-dark text-white py-5">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-3 col-12">
+                    <h4 class="font-rubik fs-20">Phone store</h4>
+                    <p class="fs-14 text-white-50">Đồ án quản lý cửa hàng điện thoại</p>
+                    <form class="row g-2">
+                        <div class="col">
+                            <button type="submit" class="btn btn-primary mb-2">Reload</button>
+                        </div>
+                    </form>
+                </div>
+                <div class="col-lg-4 col-12">
+                    <h4 class="font-rubik fs-20">Giáo viên hướng dẫn</h4>
+                    <p class="fs-14 text-white-50">Thầy Nguyễn Thanh Sang</p>
+                </div>
+                <div class="col-lg-2 col-12">
+                    <h4 class="font-rubik fs-20">Members</h4>
+                    <div class="d-flex flex-column flex-wrap">
+                        <a href="#" class="text-white-50 pb-1">Võ Duy Luân</a>
+                        <a href="#" class="text-white-50 pb-1">Bùi Lê Bích Nhung</a>
+                        <a href="#" class="text-white-50 pb-1">Dương Văn Minh Vy</a>
+                        <a href="#" class="text-white-50 pb-1">Nguyễn Thanh Thịnh</a>
+                        <a href="#" class="text-white-50 pb-1">Phạm Nguyễn Phước Thiện</a>
+                    </div>
+                </div>
+                <div class="col-lg-2 col-12">
+                    <h4 class="font-rubik fs-20">MSSV</h4>
+                    <div class="d-flex flex-column flex-wrap">
+                        <a href="#" class="text-white-50 pb-1">mssv-1</a>
+                        <a href="#" class="text-white-50 pb-1">mssv-2</a>
+                        <a href="#" class="text-white-50 pb-1">mssv-3</a>
+                        <a href="#" class="text-white-50 pb-1">mssv-4</a>
+                        <a href="#" class="text-white-50 pb-1">3121410469</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </footer>
+    <div class="bg-dark text-white py-2 text-center">
+        <p class="fs-14">&copy;Phone Store Web Project - Presented by 13th group, Web Advance 2024</p>
+    </div>
+
 </body>
 
 </html>
-
-<script>
-    $(document).ready(function() {
-
-
-    });
-</script>
