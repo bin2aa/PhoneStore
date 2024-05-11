ee1bcba74edfe70515a25666bda78c532202d5be
Catt3e <135534799+Catt3e@users.noreply.github.com>
2024-05-11 14:31:55 +0700

    Update min

--- admin/view/warehouseView.php
+++ admin/view/warehouseView.php
@@ -8,7 +8,11 @@
     <!-- Thư viện BootStrap -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
     <title>Danh sách phiếu nhập kho</title>
-
+    <style>
+        #detailWarehouseReceiptContainer{
+            min-width: 70%;
+        }
+    </style>
 </head>
 
 <body>
